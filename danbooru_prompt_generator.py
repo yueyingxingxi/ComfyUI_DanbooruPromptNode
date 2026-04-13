c272f501458f20a5417703aae3a07ce96d6b940c
yueyingxingxi <1755367233@qq.com>
2026-04-13 22:42:45 +0800

    update date

--- danbooru_prompt_generator.py
+++ danbooru_prompt_generator.py
@@ -61,7 +61,7 @@ class DanbooruPromptGenerator:
                 "custom_character": ("STRING", {
                     "multiline": True, 
                     "default": "yousa\nred eyes\nblack hair\n(DMB_hair:1.1)",
-                    "placeholder": "关闭character_default时输入自定义人物标签\n默认yousa, 1girl solo, red eyes, (DMB_hair:1.1)"
+                    "placeholder": "关闭character_default时输入自定义人物标签\n默认yousa, 1girl solo, red eyes, (DMB_hair:1.1), looking at viewer"
                 }),
                 
                 # 3. R18标签自定义输入
@@ -156,7 +156,7 @@ class DanbooruPromptGenerator:
     ]
     
     # === 人物预设 (默认) ===
-    CHARACTER_DEFAULT = ["yousa", "1girl", "solo", "red eyes", "black hair", "(DMB_hair:1.1)"]
+    CHARACTER_DEFAULT = ["yousa", "1girl", "solo", "red eyes", "black hair", "(DMB_hair:1.1)", "looking at viewer"]
     
     # === R18标签池  ===
     R18_POOL = [
